e09b849a423dc2e86566ea5f9d279ae2b57dc8a9
erkankaradag <erkankaradagcom@gmail.com>
2025-01-09 01:56:10 +0300

    Add project files.

---
+++ BabyClass/BabyClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyClass
+{
+    public class BabyClass
+    {
+
+        // Default Constructor 
+        public BabyClass()
+        {
+           
+        }
+        // Parametreli Constructor
+        public BabyClass(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            Console.WriteLine("Ingaaaaaaaaaa");
+            dateTime = DateTime.Now;
+        }
+        private string name;
+        private string surname;
+        private DateTime dateTime;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public DateTime DateBirth
+        {
+            get { return dateTime; }
+            set { dateTime = value; }
+        }
+
+        // DAHA ÖNCE DOĞAN BİR BEBEĞİN DEFAULT OLARAK BİLGİSİ GİRİLDİĞİ ZAMAN DOĞUM TARİHİNDE EKRANA OTOMATİK BASILAN 00:00:00 'I KALDIRMAK İÇİN BU METODU KULLANDIM.
+        public void PrintScreen()
+        {
+            Console.WriteLine($"Adı Soyadı: {name} {surname}, doğum tarihi {DateBirth:dd/MM/yyyy}");
+        }
+
+        // ANLIK DOĞUM TARİHİ VE BEBEK BİLGİSİ.
+        public void PrintScreen2()
+        {
+            Console.WriteLine($"Adı Soyadı: {name} {surname}, doğum tarihi {dateTime}");
+        }
+    }
+}
---
+++ BabyClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BabyClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------Parametresiz Constructor------------------");
+            BabyClass newbornBaby = new BabyClass
+            {
+                Name = "Adem",
+                Surname = "Demir",
+                DateBirth = new DateTime(2025, 01, 01)
+            };
+            newbornBaby.PrintScreen();
+
+            Console.WriteLine("------------------Parametreli Constructor---------------------");
+
+            BabyClass newbornBaby2 = new BabyClass("Arif","Çiçek");
+            newbornBaby2.PrintScreen2();
+
+
+
+
+
+        }
+    }
+}
